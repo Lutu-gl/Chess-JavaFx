9a67029d7e02997012f70e13df7ac9ad6660ec56
Lutu <luca.dris@web.de>
2021-04-01 22:57:54 +0200

    done Queen getMoves and Kind getMoves

--- ChessPrototype2.0/src/model/pieces/King.java
+++ ChessPrototype2.0/src/model/pieces/King.java
@@ -1,5 +1,6 @@
 package model.pieces;
 
+import model.Chessboard;
 import model.Color;
 import model.Field;
 
@@ -10,8 +11,42 @@ public class King extends Piece{
         super(color, name, field, value, shortName);
     }
 
+    private Chessboard chessboard;
+    private ArrayList<Field> availableMoves;
+
     @Override
     public ArrayList<Field> getMoves() {
-        return null;
+        availableMoves = new ArrayList<>();
+        chessboard = Chessboard.getInstance();
+        int line = field.getLine(), column = field.getColumn();
+
+        for (int i = -1; i < 2; i++) {
+            evaluate(line+1, column+i);
+            evaluate(line-1, column+i);
+            if(i != 0){
+                evaluate(line, column+i);
+            }
+        }
+
+        return availableMoves;
+    }
+
+    private boolean evaluate(int line, int column){
+        if(line >= chessboard.getFields().length || column >= chessboard.getFields().length || line < 0 || column < 0) return false;
+        Field nextField = chessboard.getFields()[line][column];
+
+        if(nextField.isExists()){
+            if(nextField.hasPiece()){
+                if(!nextField.getPiece().getColor().equals(this.color)){ //Wenns nit dieselbe Farbe hat kann man schlagen
+                    availableMoves.add(nextField);
+                }
+                return false;
+            }else{
+                availableMoves.add(nextField);
+                return true;
+            }
+        }else{ //Wenns nit existiert
+            return false;
+        }
     }
 }
