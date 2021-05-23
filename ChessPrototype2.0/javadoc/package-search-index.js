2f7859cf18c2b1128b4e51377cbc989a6a56dfb8
Lutu <luca.dris@web.de>
2021-05-23 18:48:34 +0200

    Added JavaDoc

---
+++ ChessPrototype2.0/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.ai"},{"l":"model.openingBook"},{"l":"model.pieces"},{"l":"view"}];updateSearchResults();
